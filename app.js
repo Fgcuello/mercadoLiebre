5147256cb817db801e4f16d7e16fc0406cd53419
unknown <cuello407@gmail.com>
2021-10-21 23:29:42 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require ("express");
+const app = express();
+const path = require ("path");
+
+const port = process.env.PORT || 3000;
+
+app.use("/public", express.static("public"));
+
+app.get("/home", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.resolve(__dirname, "views/home.html"));
+});
+
+app.listen(port, ()=>{
+    console.log("inicio servidor");
+});
